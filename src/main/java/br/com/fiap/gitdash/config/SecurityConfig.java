0e3809904f5a6011a93819a515c6645fb8e3b1c0
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-11 09:08:13 -0300

    Exercicio gitDash usando spring.security.2Oauth

---
+++ src/main/java/br/com/fiap/gitdash/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package br.com.fiap.gitdash.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/oauth2/authorization/github").permitAll()
+                        );
+
+        return http.build();
+    }
+
+}
--- src/main/java/br/com/fiap/gitdash/github/GitHubController.java
+++ src/main/java/br/com/fiap/gitdash/github/GitHubController.java
@@ -1,5 +1,7 @@
 package br.com.fiap.gitdash.github;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,11 +20,14 @@ public class GitHubController {
     }
 
     @GetMapping("/")
-    public String getUserInfo(Model model, @RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient authorizedClient) {
+    public String getUserInfo(Model model, @RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient authorizedClient, @AuthenticationPrincipal OAuth2User user) {
 
         String tokenValue = authorizedClient.getAccessToken().getTokenValue();
         List<RepositoryInfo> repos = gitHubService.getUserRepositories(tokenValue);
 
+
+        model.addAttribute("nome", user.getAttributes().get("name"));
+        model.addAttribute("avatar", user.getAttributes().get("avatar_url"));
         model.addAttribute("repos", repos);
 
         return "user";
